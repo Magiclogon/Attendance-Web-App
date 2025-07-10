de41dc9741bbaab69e23147f810a0de6c5df7b5d
Walid <walidhousni45@gmail.com>
2025-07-10 19:53:51 +0100

    Test

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -5,6 +5,6 @@ export const config = {
     companyName: 'Your company',
     employeeName: "Your name?",
     employeeEmail: 'youremail@gmail.com',
-    API_BASE_URL: 'https://backendattendanceapp.loca.lt/api'
+    API_BASE_URL: 'https://84.235.237.132:8443/api'
   };
   
